b487da6ddcedb0f26d2597f5aa3326a386109f97
Kabir Olawore <horlaworey@gmail.com>
2022-12-25 11:27:25 +0100

    Initialized 3.15 database step3

---
+++ 3.15 database step3/models/person.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+
+console.log(`Connecting to ${url}`);
+
+mongoose
+  .connect(url)
+  .then((_result) => console.log("connected to MongoDB"))
+  .catch((err) => console.error(`error connecting to MongoDB ${err}`));
+
+const phonebookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+phonebookSchema.set("toJSON", {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Person", phonebookSchema);
