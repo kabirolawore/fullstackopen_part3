d71ffd273d36a65ee3327a3b20657af89e093b29
Kabir Olawore <horlaworey@gmail.com>
2022-12-27 18:31:55 +0100

    3.19 database step 7 added

--- 3.19* database step7/index.js
+++ 3.19* database step7/index.js
@@ -100,8 +100,6 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: "malformatted or deleted id" });
   else if (error.name === "ValidationError")
     return response.status(400).json({ error: error.message });
-  // else if (error.name === "TypeError")
-  //   return response.status(400).json({ error: error.message });
 
   next(error);
 };
